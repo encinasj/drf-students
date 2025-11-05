63444a9336f1868e653ad573a22a657d413429dc
Encinas <jose.encinas@gmail.com>
2025-11-04 17:55:34 -0700

    useContext Hook

--- frontend-react/src/components/ChaildC.jsx
+++ frontend-react/src/components/ChaildC.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
+import {useContext} from 'react'
 import { StockContext, UserContext } from '../App'
 
 function ChaildC() {
+  const stockData = useContext(StockContext)
+  const userData = useContext(UserContext)
   return (
     <>
-      <StockContext.Consumer>
-        {({ stock, price }) => {
-          return (
-            <UserContext.Consumer>
-              {
-                ({ user }) => 
-                  {
-                    return (
-                      <>
-                        <h2>User - {user.name}</h2>
-                        <h2>isLoggedIn - {user.isLoggedIn}</h2>
-                        <h2>Chaild C - {stock}: {price}</h2>
-                      </>
-                    )
-                  }
-              }
-            </UserContext.Consumer>
-          )
-        }}
-      </StockContext.Consumer>
+    {/*
+        <StockContext.Consumer>
+          {({ stock, price }) => {
+            return (
+              <UserContext.Consumer>
+                {
+                  ({ user }) => 
+                    {
+                      return (
+                        <>
+                          <h2>User - {user.name}</h2>
+                          <h2>isLoggedIn - {user.isLoggedIn}</h2>
+                          <h2>Chaild C - {stock}: {price}</h2>
+                        </>
+                      )
+                    }
+                }
+              </UserContext.Consumer>
+            )
+          }}
+        </StockContext.Consumer>
+    */}
+     <h2>User - {userData.user.name}</h2>
+     <h2>Is Logeed In? - {userData.user.isLoggedIn}</h2>
+     <h2>ChaildC - {stockData.stock}</h2>
     </>
   )
 }
