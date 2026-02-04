6e43f7edc7f2a6c164cbf7b8862195d6d7746fff
Encinas <jose.encinas@gmail.com>
2026-02-04 16:12:01 -0700

    how to handle react basic form

--- frontend-react/src/components/LearnForms.jsx
+++ frontend-react/src/components/LearnForms.jsx
@@ -2,25 +2,41 @@ import React, { useState } from 'react'
 
 const LearnForms = () => {
 
+    /** 
     const [firstName, SetFirstName] = useState('')
     const [lastName, SetLastName] = useState('')
 
 
-    const handlefirstName = (e) =>{
-        SetFirstName(e.target.value)
+     *  const handlefirstName = (e) =>{
+            SetFirstName(e.target.value)
+        }
+        const handlelastName = (e) =>{
+            SetLastName(e.target.value)
+        }
+    **/
+    const [formData, SetFormData] = useState({
+        firstName:'',
+        lastName:''
+    })
+
+    const hanldeChange = (e) =>{
+        SetFormData({...formData, [e.target.name]: e.target.value})
     }
-    const handlelastName = (e) =>{
-        SetLastName(e.target.value)
+    const handleFormSubmit = (e)=>{
+        e.preventDefault();
+        console.log('From submite: ', formData);
     }
   return (
     <>
     <h1>Learn Form</h1>
     <hr />
     <br />
-    <form action="">
-        First Name: <input type="text" name='firstName' onChange={handlefirstName} defaultValue={"asdasd"} />
+    <form action="" onSubmit={handleFormSubmit}>
+        First Name: <input type="text" name='firstName' onChange={hanldeChange} defaultValue={formData.firstName} />
         <br /><br />
-        Last Name: <input type="text" name='lastName' onChange={handlelastName} defaultValue={"asdasd"}/>
+        Last Name: <input type="text" name='lastName' onChange={hanldeChange} defaultValue={formData.lastName}/>
+        <br /> <br />
+        <input type="submit" value="Submit" />
     </form>
 
     </>
